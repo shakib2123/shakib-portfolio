d15a4aaf0c1cdcd409164006697de34b92347e40
shakib2123 <mshakibalhasan21@gmail.com>
2023-12-12 16:13:54 +0600

    email sending method added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,39 @@
-import { FaArrowRight, FaMailBulk, FaPhone } from "react-icons/fa";
+import {
+  FaArrowRight,
+  FaFacebook,
+  FaInstagram,
+  FaMailBulk,
+  FaPhone,
+  FaTwitter,
+} from "react-icons/fa";
 import bgImg from "../assets/img/color-sharp.png";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const [success, setSuccess] = useState(null);
+  const ref = useRef();
   const handleSend = (e) => {
     e.preventDefault();
-    const form = e.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const description = form.description.value;
-    const info = {
-      name: name,
-      email,
-      description,
-    };
-    console.log(info);
+
+    emailjs
+      .sendForm(
+        "service_f9xu5hu",
+        "template_izhx1g4",
+        ref.current,
+        "S6pui1mHY5mGQipH4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+          setSuccess(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setSuccess(false);
+        }
+      );
   };
   return (
     <div
@@ -36,8 +56,15 @@ const Contact = () => {
         <p className="text-gray-200 flex gap-2 items-center mt-2 text-lg">
           <FaPhone /> +8801641991384
         </p>
+        <p className="text-gray-100 text-2xl mt-3 flex gap-4 items-center">
+          <a href="https://web.facebook.com/profile.php?id=100092629614657">
+            <FaFacebook />
+          </a>
+          <FaTwitter></FaTwitter>
+          <FaInstagram />
+        </p>
       </div>
-      <form onSubmit={handleSend} className="w-full">
+      <form ref={ref} onSubmit={handleSend} className="w-full">
         <div className="mb-6 w-full">
           <label className="block mb-2 text-sm font-medium text-green-300 ">
             Your name
@@ -66,14 +93,22 @@ const Contact = () => {
           </label>
           <textarea
             type="text"
-            name="description"
+            name="message"
             className="bg-green-50 border border-green-500 text-green-800  placeholder-green-700 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5"
             placeholder="Your name.."
           />
         </div>
-        <button className="flex justify-center items-center gap-2 px-4 py-3 w-full text-gray-300 font-semibold">
+        <button
+          type="submit"
+          className="flex justify-center items-center gap-2 px-4 py-3 w-full text-gray-300 font-semibold"
+        >
           Send <FaArrowRight />
         </button>
+        {success && (
+          <p className="text-lg text-green-700 text-center mt-4">
+            You will get the reply very soon
+          </p>
+        )}
       </form>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,9 @@
-import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import {
+  FaCopyright,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+} from "react-icons/fa";
 import footerImg from "../assets/img/footer-bg.png";
 const Footer = () => {
   return (
@@ -6,17 +11,13 @@ const Footer = () => {
       style={{ backgroundImage: footerImg }}
       className="bg-cover bg-center h-40 overflow-hidden px-3 pt-8"
     >
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
         <p className="text-gray-300 text-3xl font-bold flex gap-2 items-center">
           <img className="w-24" src="/logo.png" alt="" />
           SHAKIB
         </p>
-        <p className="text-gray-100 text-2xl flex gap-4 items-center">
-          <a href="https://web.facebook.com/profile.php?id=100092629614657">
-            <FaFacebook />
-          </a>
-          <FaTwitter></FaTwitter>
-          <FaInstagram />
+        <p className="flex gap-2 text-gray-300 items-center">
+          Developed with love by Shakib <FaCopyright /> 2023
         </p>
       </div>
     </div>
